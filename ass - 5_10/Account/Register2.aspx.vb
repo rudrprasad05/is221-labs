b564bf2410759c4a8e00c534759d94fad2516cca
Rudr Prasad <84302015+rudrprasad05@users.noreply.github.com>
2024-05-10 19:39:58 +1200

    Add files via upload

---
+++ ass - 5_10/Account/Register2.aspx.vb
@@ -0,0 +1,12 @@
+﻿
+Partial Class Account_Register2
+    Inherits System.Web.UI.Page
+
+    Protected Sub btnAddUser_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnAddUser.Click
+       
+        addUser.InsertParameters("User_Name").DefaultValue = txtUsername.Text
+        addUser.InsertParameters("User_Passwd").DefaultValue = txtPassword.Text()
+        addUser.InsertParameters("User_Prog").DefaultValue = txtPassword.Text()
+        addUser.Insert()
+    End Sub
+End Class
